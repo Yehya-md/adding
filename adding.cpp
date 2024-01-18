0ad3c689f84ee470e9cf936db796229405b94c31
Yehya muhammed <muhammedyehya60@example.com>
2024-01-18 21:41:34 +0300

    first commit

---
+++ adding.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int num1, num2;
+    int sum;
+    cin >> num1 >> num2;
+    sum = add(num1, num2);
+    cout << sum;
+    return 0;
+}
